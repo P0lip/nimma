a23b8e4c1f58ed33e274e5649047b8de9e4defbf
Jakub Rożek <jakub@rozek.tech>
2021-09-03 21:53:49 +0200

    refactor(runtime): redundant loop in inBounds codegen fn

--- src/runtime/codegen-functions/in-bounds.mjs
+++ src/runtime/codegen-functions/in-bounds.mjs
@@ -1,14 +1,12 @@
 export default function (value, pos, start, end, step) {
   if (!Array.isArray(value)) return false;
 
-  const len = value.length;
-  start = start < 0 ? Math.max(0, start + len) : Math.min(len, start);
-  end = end < 0 ? Math.max(0, end + len) : Math.min(len, end);
-  for (let i = start; i < end; i += step) {
-    if (i === pos) {
-      return true;
-    }
-  }
+  const actualStart =
+    start < 0
+      ? Math.max(0, start + value.length)
+      : Math.min(value.length, start);
+  const actualEnd =
+    end < 0 ? Math.max(0, end + value.length) : Math.min(value.length, end);
 
-  return false;
+  return pos >= actualStart && pos < actualEnd && (pos + start) % step === 0;
 }
