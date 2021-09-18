733617787d3d9df05ec7c2a6f10db729d57e0c12
Jakub Rożek <jakub@rozek.tech>
2021-09-19 01:24:45 +0300

    chore(runtime): remove unused noop fn

--- src/runtime/codegen-functions/index.mjs
+++ src/runtime/codegen-functions/index.mjs
@@ -1,4 +1,3 @@
 export { default as get } from './get.mjs';
 export { default as inBounds } from './in-bounds.mjs';
 export { default as isObject } from './is-object.mjs';
-export { default as noop } from './noop.mjs';
