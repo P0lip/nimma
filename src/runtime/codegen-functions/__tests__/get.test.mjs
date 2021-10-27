391efd64a4996c2cc227e191d2f9b1706c7115b4
Jakub Rożek <jakub@rozek.tech>
2021-10-28 00:47:46 +0300

    test(runtime): cover get runtime fn

---
+++ src/runtime/codegen-functions/__tests__/get.test.mjs
@@ -0,0 +1,35 @@
+import chai from 'chai';
+
+import get from '../get.mjs';
+
+const { expect } = chai;
+
+describe('get codegen function', () => {
+  it('should gracefully handle invalid input', () => {
+    expect(get(null, [])).to.be.null;
+  });
+
+  it('should gracefully handle missed values', () => {
+    expect(
+      get(
+        {
+          foo: {
+            bar: true,
+          },
+        },
+        ['foo', 'bar', 'baz', 'oops'],
+      ),
+    ).to.be.undefined;
+  });
+
+  it('should retrieve value', () => {
+    const input = {
+      foo: {
+        bar: true,
+      },
+    };
+    expect(get(input, [])).to.eq(input);
+    expect(get(input, ['foo'])).to.eq(input.foo);
+    expect(get(input, ['foo', 'bar'])).to.eq(input.foo.bar);
+  });
+});
