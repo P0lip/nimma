1e3f1eb94a2552b099cb2a6497614a72af9069cf
Jakub Rożek <jakub@rozek.tech>
2021-10-28 00:37:26 +0300

    chore(parser): jsep does not export Jsep member in its CJS build

--- src/parser/jsep.mjs
+++ src/parser/jsep.mjs
@@ -1,6 +1,6 @@
 import regex from '@jsep-plugin/regex';
 import ternary from '@jsep-plugin/ternary';
-import { Jsep } from 'jsep';
+import Jsep from 'jsep';
 
 Jsep.addIdentifierChar('@');
 Jsep.addUnaryOp('void');
@@ -8,7 +8,4 @@ Jsep.addBinaryOp('in', 12);
 Jsep.addBinaryOp('~=', 20);
 Jsep.plugins.register(regex, ternary);
 
-export default a => {
-  const jsep = new Jsep(a);
-  return jsep.parse();
-};
+export default expr => Jsep.parse(expr);
